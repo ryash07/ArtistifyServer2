4f6f02a0136ba93db93153cdf10f2db2b6e76876
Uzzal-Bhowmik <ubcreativity111@gmail.com>
2024-02-21 18:01:49 +0600

    cloudinary upload api added

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ const { calculateComparingPercentage } = require("./calculatePercentageChange");
 const { formatSalesData } = require("./formatSalesData");
 const stripe = require("stripe")(process.env.PAYMENT_GATEWAY_SECRET_KEY);
 const jwt = require("jsonwebtoken");
+const cloudinary = require("cloudinary").v2;
 
 // middlewares
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "200mb" }));
 
 // Verify JWT Token Middleware
 const verifyJWT = (req, res, next) => {
@@ -34,6 +35,13 @@ const verifyJWT = (req, res, next) => {
     next();
   });
 };
+
+// Cloudinary Config
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
 
 // MongoDB Connection
 const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@cluster0.5732rtt.mongodb.net/?retryWrites=true&w=majority`;
@@ -168,6 +176,28 @@ async function run() {
       };
       const result = await userCollection.updateOne(filter, updatedDoc);
       res.send(result);
+    });
+
+    // UPLOAD USER IMAGE TO CLOUDINARY
+    app.post("/cloudinary-upload", async (req, res) => {
+      const { name, img } = req.body;
+
+      cloudinary.uploader.upload(
+        img,
+        {
+          public_id: name,
+          resource_type: "image",
+          overwrite: false,
+          folder: "ub-jewellers-users-pic",
+        },
+        function (error, result) {
+          if (error) {
+            return res.send({ success: false, error: error });
+          }
+
+          return res.send({ success: true, imgURL: result.secure_url });
+        }
+      );
     });
 
     // NAV NOTIFICATIONS GET METHOD
